5484d603c5acb7d8f615519f729668a65f32bf88
zDefcon20 <antholozadarequelme@gmail.com>
2025-01-06 14:57:15 -0500

    first commit

---
+++ Variables.lua
@@ -0,0 +1,20 @@
+-- Variable global
+varGlobal = 10
+
+-- Variable local
+local varLocal = 20
+
+-- Imprimir
+print("Variable global: " .. varGlobal) -- output: 10
+print("Variable local: " .. varLocal) -- output: 20
+
+-- Variable numerica
+local numero = 40
+
+-- Variable string
+local cadena = "Hola, mundo"
+
+--Variable booleana
+local booleano = true
+
+-- Variable de tabla (similar a array o diccionario)
